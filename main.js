a60743ceac45b6ba872ec9be840ec1fbf682a964
Poonam Pande <p.pande@earthlink.net>
2023-10-24 21:21:45 -0400

    WIP

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 let customerContainer = document.querySelector('.container')
 
-
 function customerDirectoryCard(customer) {
     let customerCard = document.createElement('div');
     let customerPicture = document.createElement('img');
     customerPicture.src = `${customer.picture.large}`;
     let customerName = document.createElement('h2');
     customerName.innerText = `${customer.name.first} ${customer.name.last}`;
-    let customerEmail = document.createElement('mailto');
+    let customerEmail = document.createElement('a');
+    customerEmail.href = "mailto";
     customerEmail.innerHTML = `${customer.email}`;
     let customerStreet= document.createElement('h3');
     customerStreet.innerText = `${customer.location.street.number} ${customer.location.street.name}`;
@@ -15,7 +15,7 @@ function customerDirectoryCard(customer) {
     customerCity.innerText = `${customer.location.city}, ${customer.location.state} ${customer.location.postcode}`;
     let customerDob = document.createElement('h5');
     customerDob.innerText = `DOB: ${customer.dob.date}`;
-    customerRegistered = document.createElement('h6');
+    customerRegistered = document.createElement('date');
     customerRegistered.innerText = `Customer since: ${customer.registered.date}`;
     customerCard.appendChild(customerPicture);
     customerCard.appendChild(customerName);
